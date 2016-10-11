85d79643c926c2971f3226fcaf8a1bd2b75daf41
cheneym <cheneymao94@gmail.com>
2016-10-11 14:40:58 -0700

    Use passport for user verification

--- shortly.js
+++ shortly.js
@@ -2,8 +2,8 @@ var express = require('express');
 var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
-var session = require('express-session');
 var bcrypt = require('bcrypt-nodejs');
+var cookieParser = require('cookie-parser');
 
 var db = require('./app/config');
 var Users = require('./app/collections/users');
@@ -11,7 +11,10 @@ var User = require('./app/models/user');
 var Links = require('./app/collections/links');
 var Link = require('./app/models/link');
 var Click = require('./app/models/click');
-
+var passport = require('passport');
+var session = require('express-session');
+var GitHubStrategy = require('passport-github2').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 var app = express();
 
 app.set('views', __dirname + '/views');
@@ -22,64 +25,130 @@ app.use(bodyParser.json());
 // Parse forms (signup/login)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
+
+app.use(cookieParser());
 app.use(session({
   secret: 'password',
-  resave: false,
+  resave: true,
   saveUninitialized: true
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-var restrict = function(req, res, next) {
-  // console.log('request session', req.session);
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
 
-  if (req.session.user) {
-    next();
-  } else {
-    req.session.error = 'Access denied!';
-    res.redirect('/login');
-  } 
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+var validateUserPass = function(username, password, done) {
+  return new User({username: username}).fetch()
+    .then(function(found) {
+      if (found) { 
+        var salt = found.attributes.salt;
+        var encryptpw = found.attributes.password;
+        if (bcrypt.hashSync(password, salt) === encryptpw) {
+          return done(null, found.attributes);
+        } else {
+          return done(null, false, { message: 'Incorrect username or password.' });
+        }
+      } else {
+        return done(null, false, { message: 'Incorrect username or password.' });
+      }
+    });
 };
 
-app.get('/', restrict,
+passport.use(new LocalStrategy(validateUserPass));
+
+var loggedIn = function(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+};
+
+// passport.use(new GoogleStrategy({
+//   consumerKey: GOOGLE_CONSUMER_KEY,
+//   consumerSecret: GOOGLE_CONSUMER_SECRET,
+//   callbackURL: "http://www.example.com/auth/google/callback"
+// },
+//   function(token, tokenSecret, profile, done) {
+//     User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//       return done(err, user);
+//     });
+//   }
+// ));
+
+// app.get('/auth/google',
+//   passport.authenticate('google', { scope: 'https://www.google.com/m8/feeds' });
+
+// app.get('/auth/google/callback', 
+//   passport.authenticate('google', { failureRedirect: '/login' }),
+//   function(req, res) {
+//     res.redirect('/');
+//   });
+
+
+// passport.use(new GitHubStrategy({
+//   clientID: config.github.clientID,
+//   clientSecret: config.github.clientSecret,
+//   callbackURL: config.github.callbackURL
+// }, function(accessToken, refreshToken, profile, done) {
+
+//   var searchQuery = {
+//     name: profile.displayName
+//   };
+
+//   var updates = {
+//     name: profile.displayName,
+//     someID: profile.id
+//   };
+
+//   var options = {
+//     upsert: true
+//   };
+
+//   // update the user if s/he exists or add a new user
+//   User.findOneAndUpdate(searchQuery, updates, options, function(err, user) {
+//     if (err) {
+//       return done(err);
+//     } else {
+//       return done(null, user);
+//     }
+//   });
+// }
+
+// ));
+
+
+app.get('/', loggedIn,
 function(req, res) {
   res.render('index');
 });
 
-app.get('/create', restrict,
+
+app.get('/create', loggedIn,
 function(req, res) {
   res.render('index');
 });
 
-app.get('/links', restrict,
+
+app.get('/links', loggedIn,
 function(req, res) {
-  var username = req.session.user;
-  new User({ username: username }).fetch()
-    .then(function(found) {
-      Links.reset().fetch().then(function(links) {
-        var models = links.models.filter(function(model) {
-          return model.attributes.user_id === found.attributes.id;
-        });
-        res.status(200).send(models);
-        // links({'user_id': found.attributes.id}).fetch()
-        // .then(function(found) {
-        //   console.log(found);
-        //   res.status(200).send(found);
-        // });
-      });
-      // new Link({'user_id': found.attributes.id}).fetch()
-      //   .then(function(found) {
-      //     console.log(found);
-      //     res.status(200).send(found);
-      //   });
+  Links.reset().fetch().then(function(links) {
+    var models = links.models.filter(function(model) {
+      return model.attributes.user_id === req.user.id;
     });
+    res.status(200).send(models);
+  });
 });
 
 app.get('/login', function(req, res) {
   res.render('login');
-});
-
-app.get('/signup', function(req, res) {
-  res.render('signup');
 });
 
 app.get('/logout', function(req, res) {
@@ -87,6 +156,11 @@ app.get('/logout', function(req, res) {
     res.redirect('/login');
   });
 });
+
+app.get('/signup', function(req, res) {
+  res.render('signup');
+});
+
 
 app.post('/signup', function(req, res) {
   var username = req.body.username;
@@ -122,29 +196,12 @@ app.post('/signup', function(req, res) {
   });
 });
 
-app.post('/login', function(req, res) {
-  var username = req.body.username;
-  var password = req.body.password;
-
-  new User({username: username}).fetch()
-    .then(function(found) {
-      if (found) {
-        var salt = found.attributes.salt;
-        var encryptpw = found.attributes.password;
-        if (bcrypt.hashSync(password, salt) === encryptpw) {
-          req.session.user = found.attributes.username;
-          res.redirect('/');          
-        } else {
-          console.log('invalid username or password');
-          res.redirect('/login');
-        }
-      } else {
-        console.log('invalid username or password');
-        res.redirect('/login');
-      }
-    });
-});
-
+app.post('/login', passport.authenticate('local', 
+  { 
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true 
+  }));
 
 app.post('/links', 
 function(req, res) {
@@ -155,7 +212,7 @@ function(req, res) {
     return res.sendStatus(404);
   }
 
-  new Link({ url: uri }).fetch().then(function(found) {
+  new Link({ url: uri, 'user_id': req.user.id }).fetch().then(function(found) {
     if (found) {
       res.status(200).send(found.attributes);
     } else {
@@ -164,19 +221,15 @@ function(req, res) {
           console.log('Error reading URL heading: ', err);
           return res.sendStatus(404);
         }
-        var username = req.session.user;
-        new User({ username: username }).fetch()
-          .then(function(found) {
-            Links.create({
-              url: uri,
-              title: title,
-              baseUrl: req.headers.origin,
-              'user_id': found.attributes.id
-            })
-            .then(function(newLink) {
-              res.status(200).send(newLink);
-            });
-          });
+        Links.create({
+          url: uri,
+          title: title,
+          baseUrl: req.headers.origin,
+          'user_id': req.user.id
+        })
+        .then(function(newLink) {
+          res.status(200).send(newLink);
+        });
       });
     }
   });
